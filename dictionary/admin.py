172886b3de573fd44c0ec6231b3e20862b30d379
Seth Van Roekel <exomut@gmail.com>
2020-01-02 18:19:11 +0900

    Basic SQLite model setup.

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Entries
+
+admin.site.register(Entries)
+
+
--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Entries(models.Model):
+    japanese = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.japanese
